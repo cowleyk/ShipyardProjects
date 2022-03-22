4ca396b114a965d6fbdf80577508b7cfb4b2e27a
Kevin Cowley <cowley.kevin.l@gmail.com>
2022-03-21 21:38:24 -0600

    :fire: Remove console logging from ICO

--- lp/contracts/ICO.sol
+++ lp/contracts/ICO.sol
@@ -5,7 +5,6 @@ import "./SpaceCoin.sol";
 import "./interfaces/IRouter.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "hardhat/console.sol";
 
 /// @title A contract for raising ICO funds
 /// @author Kevin Cowley
